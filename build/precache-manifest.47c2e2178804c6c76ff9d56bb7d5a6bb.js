4bccbb2e8b849b07e3a48f823b557caf45eb17dc
youngfoe <alexfoderaro03@gmail.com>
2021-02-05 15:36:09 +0100

    ...

--- build/precache-manifest.47c2e2178804c6c76ff9d56bb7d5a6bb.js
+++ build/precache-manifest.47c2e2178804c6c76ff9d56bb7d5a6bb.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c8ca1d26e47c69c662a5e72e5b8bad56",
-    "url": "../index.html"
+    "url": "/liquidity-snatchers/index.html"
   },
   {
     "revision": "fe4bca10d6aa207fe557",
-    "url": "../static/css/main.99e75adc.chunk.css"
+    "url": "/liquidity-snatchers/static/css/main.99e75adc.chunk.css"
   },
   {
     "revision": "2e93f04c3a7401183129",
-    "url": "../static/js/2.4f13ed7b.chunk.js"
+    "url": "/liquidity-snatchers/static/js/2.4f13ed7b.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "../static/js/2.4f13ed7b.chunk.js.LICENSE.txt"
+    "url": "/liquidity-snatchers/static/js/2.4f13ed7b.chunk.js.LICENSE.txt"
   },
   {
     "revision": "fe4bca10d6aa207fe557",
-    "url": "../static/js/main.08012e14.chunk.js"
+    "url": "/liquidity-snatchers/static/js/main.08012e14.chunk.js"
   },
   {
     "revision": "592cf5197973f4866da3",
-    "url": "../static/js/runtime-main.ac8fcdd7.js"
+    "url": "/liquidity-snatchers/static/js/runtime-main.ac8fcdd7.js"
   },
   {
     "revision": "be36452223389535b1d70d7a3afbec92",
-    "url": "../static/media/price.be364522.mp4"
+    "url": "/liquidity-snatchers/static/media/price.be364522.mp4"
   },
   {
     "revision": "fc4fa17527541d88aa01fa391ff85927",
-    "url": "../static/media/svg-0.fc4fa175.svg"
+    "url": "/liquidity-snatchers/static/media/svg-0.fc4fa175.svg"
   },
   {
     "revision": "2a3c12d73b2b197ff879a9818e3b88d8",
-    "url": "../static/media/svg-1.2a3c12d7.svg"
+    "url": "/liquidity-snatchers/static/media/svg-1.2a3c12d7.svg"
   },
   {
     "revision": "eae9e9e98224acd84ca3634ca9258adb",
-    "url": "../static/media/svg-2.eae9e9e9.svg"
+    "url": "/liquidity-snatchers/static/media/svg-2.eae9e9e9.svg"
   },
   {
     "revision": "99d71bcf639dee620e5d6bb2c5c24688",
-    "url": "../static/media/svg-3.99d71bcf.svg"
+    "url": "/liquidity-snatchers/static/media/svg-3.99d71bcf.svg"
   },
   {
     "revision": "d8e8da85f7078942c33b6780a0d6912d",
-    "url": "../static/media/svg-4.d8e8da85.svg"
+    "url": "/liquidity-snatchers/static/media/svg-4.d8e8da85.svg"
   }
 ]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../precache-manifest.47c2e2178804c6c76ff9d56bb7d5a6bb.js"
+  "/liquidity-snatchers/precache-manifest.47c2e2178804c6c76ff9d56bb7d5a6bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/liquidity-snatchers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
